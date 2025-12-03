47c285c3c16848e9368765e59367a52726ef3999
arman455 <snositel99@gmail.com>
2025-12-03 14:47:08 +0200

    fix: remove unused Prisma models and generated files to clean up the schema and reduce bloat

--- dist/Chats/chat.type.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// acknowledgment
-// В Socket.io acknowledgment (callback) це функція, яка відправляє повідомлення в
-// результаті якоїсь події (аналог http)
--- dist/config/errorCodes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errors = void 0;
-exports.errors = {
-    P2002: "Given non unique value",
-    P2003: "Field is not found",
-    P2007: "Data validation error",
-    P2014: "Error in relations",
-};
